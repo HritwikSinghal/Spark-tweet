c450606ff41bd79feadabecdfc75f09e378a80ee
Utkarsh Gupta <utkarshgupta@Utkarshs-MacBook-Air.local>
2021-11-22 19:52:41 +0530

    Frontend Version 1

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from os import truncate
 import re
 
 from flask import Flask, jsonify, request
@@ -6,43 +7,43 @@ import ast
 
 app = Flask(__name__)
 
-labels = []
-values = []
-
+dataValues = []
+categoryValues = []
 
 @app.route("/")
-def chart():
-    return render_template('chart.html', values=values, labels=labels)
+def home():
+    return render_template('index.html',dataValues=dataValues,categoryValues=categoryValues)
 
 
 @app.route('/refreshData')
 def refresh_graph_data():
-    global labels, values
-    print("labels now: " + str(labels))
-    print("data now: " + str(values))
-    return jsonify(sLabel=labels, sData=values)
+    global dataValues, categoryValues
+    print("labels now: " + str(dataValues))
+    print("data now: " + str(categoryValues))
+    # dataValues[0]=dataValues[0]+1
+    return jsonify(dataValues=dataValues, categoryValues=categoryValues)
 
 
 @app.route('/updateData', methods=['POST'])
 def update_data_post():
-    global labels, values
+    global dataValues, categoryValues
     if not request.form or 'data' not in request.form:
         return "error", 400
-    labels = ast.literal_eval(request.form['label'])
+    categoryValues = ast.literal_eval(request.form['label'])
 
-    for i, ele in enumerate(labels):
+    for i, ele in enumerate(categoryValues):
         try:
             new_ele = re.findall(r'bytearray\(b\'(#.*)\'\)', ele)[0]
             print(new_ele)
-            labels[i] = new_ele
+            categoryValues[i] = new_ele
         except:
             continue
 
-    values = ast.literal_eval(request.form['data'])
-    print(f"labels received: {str(labels)}")
-    print(f"data received: {str(values)}")
+    dataValues = ast.literal_eval(request.form['data'])
+    print(f"labels received: {str(categoryValues)}")
+    print(f"data received: {str(dataValues)}")
     return "success", 201
 
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=5001)
+    app.run(host='localhost', port=5001 , debug=True)
